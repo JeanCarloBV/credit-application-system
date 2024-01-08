cb20c1e675b0aba439a8e0501d097e5de25da544
Jean Carlo <jcbaenavicente@gmail.com>
2024-01-08 12:29:54 -0300

    Criadas as classes CustomerDto e CustomerResource com implementação dos métodos saveCustomer, findById e deleteById

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/controller/CustomerResource.kt
@@ -0,0 +1,33 @@
+package me.dio.creditapplicationsystem.controller
+
+import me.dio.creditapplicationsystem.DTO.CustomerDto
+import me.dio.creditapplicationsystem.DTO.CustomerView
+import me.dio.creditapplicationsystem.entity.Customer
+import me.dio.creditapplicationsystem.service.impl.CustomerService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/customers")
+class CustomerResource(
+    private val customerService: CustomerService
+) {
+    @PostMapping
+    fun saveCustomer(@RequestBody customerDto: CustomerDto): String{
+        val savedCustomer = this.customerService.save(customerDto.toEntity())
+        return "Customer ${savedCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @GetMapping("/{id^}")
+    fun deleteById(@PathVariable id: Long) = this.customerService.delete(id)
+}
