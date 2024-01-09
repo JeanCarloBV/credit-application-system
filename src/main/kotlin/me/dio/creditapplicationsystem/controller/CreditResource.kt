64dd4e81bed2d5ba077ee234c554208318acf121
Jean Carlo <jcbaenavicente@gmail.com>
2024-01-09 09:17:20 -0300

    Implementação do método findAllByCustomerId

--- src/main/kotlin/me/dio/creditapplicationsystem/controller/CreditResource.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/controller/CreditResource.kt
@@ -1,12 +1,17 @@
 package me.dio.creditapplicationsystem.controller
 
 import me.dio.creditapplicationsystem.DTO.CreditDto
+import me.dio.creditapplicationsystem.DTO.CreditViewList
 import me.dio.creditapplicationsystem.entity.Credit
 import me.dio.creditapplicationsystem.service.impl.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collector
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credits")
@@ -15,5 +20,12 @@ class CreditResource(private val creditService: CreditService) {
     fun saveCredit(@RequestBody creditDto: CreditDto): String {
         val credit: Credit = this.creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    }
+
+    @GetMapping
+    fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList>{
+        return this.creditService.findAllByCustomer(customerId).stream().map {
+            credit: Credit -> CreditViewList(credit)
+        }.collect(Collectors.toList())
     }
 }
