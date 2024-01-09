0d773aad1896df7ff659ba3717194af233205854
Jean Carlo <jcbaenavicente@gmail.com>
2024-01-09 11:26:33 -0300

    Inseridas anotações de restrição dos dados inseridos

--- src/main/kotlin/me/dio/creditapplicationsystem/DTO/CreditDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/DTO/CreditDto.kt
@@ -1,5 +1,9 @@
 package me.dio.creditapplicationsystem.DTO
 
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.creditapplicationsystem.entity.Credit
 import me.dio.creditapplicationsystem.entity.Customer
 import java.math.BigDecimal
@@ -7,10 +11,10 @@ import java.time.LocalDate
 import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Private
 
 class CreditDto(
-    private val creditValue: BigDecimal,
-    private val dayFirstOfInstallment: LocalDate,
-    private val numberOfInstallments: Int,
-    private val customerId: Long
+    @field:NotNull(message = "Invalid input") val creditValue: BigDecimal,
+    @field:Future(message = "Invalid input") val dayFirstOfInstallment: LocalDate,
+    @field:Max(value = 48, message = "Max installments number exceeded") val numberOfInstallments: Int,
+    @field:NotNull(message = "Invalid input") val customerId: Long
 ) {
     fun toEntity(): Credit = Credit(
         creditValue = this.creditValue,
