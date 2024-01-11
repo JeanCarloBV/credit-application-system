3c9ad23da2d21d7fda10c41146b8697ae2f274d9
Jean Carlo <jcbaenavicente@gmail.com>
2024-01-11 12:28:36 -0300

    Corrigindo anotações nas classes DTO

--- src/main/kotlin/me/dio/creditapplicationsystem/DTO/CreditDto.kt
+++ src/main/kotlin/me/dio/creditapplicationsystem/DTO/CreditDto.kt
@@ -2,6 +2,7 @@ package me.dio.creditapplicationsystem.DTO
 
 import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import me.dio.creditapplicationsystem.entity.Credit
 import me.dio.creditapplicationsystem.entity.Customer
@@ -10,8 +11,8 @@ import java.time.LocalDate
 
 class CreditDto(
     @field:NotNull(message = "Invalid input") val creditValue: BigDecimal,
-    @field:Future(message = "Invalid input") val dayFirstOfInstallment: LocalDate,
-    @field:Max(value = 48, message = "Max installments number exceeded") val numberOfInstallments: Int,
+    @field:Future(message = "Invalid date") val dayFirstOfInstallment: LocalDate,
+    @field:Min(value = 1) @field:Max(value = 48, message = "Max installments number exceeded") val numberOfInstallments: Int,
     @field:NotNull(message = "Invalid input") val customerId: Long
 ) {
 
